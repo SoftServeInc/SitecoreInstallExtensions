16e7811e80621d5d8e1fc3a51fa5b7b0762a04cf
Ro Se <robert.senktas@gmail.com>
2018-03-08 15:25:57 +0100

    Add scripts
    Installation script and help generator

---
+++ install-modules.ps1
@@ -0,0 +1,49 @@
+#requires -RunAsAdministrator 
+#requires -Version 5.1
+
+#region "Register Sitecore Gallery
+if( (Get-PSRepository -Name SitecoreGallery -ErrorAction SilentlyContinue) -eq $null )
+{
+    Write-Verbose "Configure SitecoreGallery repository"
+    Register-PSRepository -Name SitecoreGallery -SourceLocation https://sitecore.myget.org/F/sc-powershell/api/v2 -InstallationPolicy Trusted
+}
+#endregion
+ 
+#region "SitecoreInstallFramework"
+if( (Get-Module -Name SitecoreInstallFramework -ListAvailable) -eq $null )
+{
+    #If install-module is not available check https://www.microsoft.com/en-us/download/details.aspx?id=49186
+    Install-Module SitecoreInstallFramework -Scope AllUsers -Repository SitecoreGallery
+}
+else
+{
+    Write-Verbose "SIF module already installed, update then"
+	Update-Module SitecoreInstallFramework -Force
+}
+#endregion
+
+#region "SitecoreFundamentals"
+if( (Get-Module -Name SitecoreFundamentals -ListAvailable) -eq $null )
+{
+    #If install-module is not available check https://www.microsoft.com/en-us/download/details.aspx?id=49186
+    Install-Module SitecoreFundamentals -Scope AllUsers -Repository SitecoreGallery
+}
+else
+{
+    Write-Verbose "SitecoreFundamentals module already installed, update then"
+	Update-Module SitecoreFundamentals -Force
+}
+#endregion
+
+#region "SitecoreInstallExtensions"
+if( (Get-Module -Name SitecoreInstallExtensions -ListAvailable) -eq $null )
+{
+    #If install-module is not available check https://www.microsoft.com/en-us/download/details.aspx?id=49186
+    Install-Module SitecoreInstallExtensions -Scope AllUsers -Repository PSGallery
+}
+else
+{
+    Write-Verbose "SitecoreInstallExtensions module already installed, update then"
+	Update-Module SitecoreInstallExtensions -Force
+}
+#region
