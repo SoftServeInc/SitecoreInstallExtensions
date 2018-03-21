3a60423e935aaf51aaf73c3deaabb87eed0ef00a
Ro Se <robert.senktas@gmail.com>
2018-03-22 00:30:23 +0100

    Invoke-InstallWindowsFeatureTask

---
+++ SitecoreInstallExtensions/Tasks/Invoke-InstallWindowsFeatureTask.ps1
@@ -0,0 +1,64 @@
+#
+# Invoke-InstallWindowsFeatureTask.ps1
+#
+Function Invoke-InstallWindowsFeatureTask {
+<#
+.SYNOPSIS
+	Installs one or more roles, role services, or features on the local server 
+
+.DESCRIPTION
+	The Invoke-InstallWindowsFeatureTask is registered as InstallWindowsFeature type.
+
+.EXAMPLE
+	Json task configuration for Sitecore Install Framework:
+
+	"InstallRequiredFeatures": {
+      "Type": "InstallWindowsFeature",
+      "Params": {
+        "FeaturesToInstall":  [
+			"Net-Framework-45-ASPNET",
+			"Web-Server",
+			"Web-Mgmt-Tools",
+		  ]
+        "XmlPath": "required-features.xml"
+      }
+    }
+)
+
+.EXAMPLE
+	$windowsFeatures = @('Net-Framework-45-ASPNET', 'Web-Server' )
+	Invoke-InstallWindowsFeatureTask -FeaturesToInstall $windowsFeatures
+
+.NOTE
+	This task requires 'servermanager' module.
+	For a client-base operating system need to install https://www.microsoft.com/en-us/download/details.aspx?id=45520
+
+#>
+	[CmdletBinding(SupportsShouldProcess=$true)]
+	param(
+		[Parameter()]
+		[string[]]$FeaturesToInstall,
+		[Parameter()]
+		[string]$XmlPath
+	)
+
+	$installed = (Get-Module servermanager -ListAvailable -ErrorAction SilentlyContinue) -ne $null
+	if( $installed -ne $true )
+	{
+		throw "Install-Module servermanager"
+	}
+
+	if( $FeaturesToInstall.Count > 0 )
+	{
+		Install-WindowsFeature -Name $FeaturesToInstall 
+	}
+
+	if( Test-Path $XmlPath )
+	{
+		Install-WindowsFeature -ConfigurationFilePath $XmlPath
+	}
+}
+
+Export-ModuleMember Invoke-InstallWindowsFeatureTask
+Register-SitecoreInstallExtension -Command Invoke-InstallWindowsFeatureTask -As InstallWindowsFeature -Type Task
+
