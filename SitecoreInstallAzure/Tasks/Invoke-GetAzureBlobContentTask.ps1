8de26a7c56d8a652a804f4d0b1c6bfc71dcc32f9
Ro Se <robert.senktas@gmail.com>
2018-05-27 00:29:46 +0200

    add functionality to download blob from Azure with SAS token

--- SitecoreInstallAzure/Tasks/Invoke-GetAzureBlobContentTask.ps1
+++ SitecoreInstallAzure/Tasks/Invoke-GetAzureBlobContentTask.ps1
@@ -68,5 +68,70 @@ function Invoke-GetAzureBlobContentTask
 	}
 }
 
+
+function Invoke-DownloadAzureBlobContentTask
+{
+     [CmdletBinding(SupportsShouldProcess=$true)]
+     Param
+     (
+        # The url to the Azure storage
+        [Parameter(Mandatory=$true)]
+		[string]
+        $Url,
+		
+		# The SAS token for storage
+		[Parameter(Mandatory=$true)]
+		[string]
+        $Token,
+
+		# The Azure container name
+        [string]
+        $Container,
+
+		# The list of files to download from Url\Container
+        [string[]]
+        $Blobs,
+		
+		# The local folder where blobs will be downloaded
+        [string]
+        $Destination
+     )
+	
+	foreach( $blob in $Blobs )		
+	{
+		$fileName = Split-Path $blob -Leaf
+		$destinationPath = Join-Path -Path $Destination -ChildPath $fileName
+
+		if($pscmdlet.ShouldProcess($Url, "Download $Container\$blob => $destinationPath"))
+		{
+			if( -not (Test-Path $destinationPath ) )
+			{
+				$startTime = Get-Date
+				Write-TaskInfo -Message "$Url\$Container\$blob => $destinationPath" -Tag 'Download'
+
+				Add-Type -AssemblyName System.Web
+				$blobUrl = $blob -replace " ","%20"
+
+				$uri = "$Url/$Container/$blobUrl$Token"
+				Invoke-WebRequest -Uri $uri -OutFile $destinationPath
+
+				$endTime = Get-Date
+				$timeTaken = $endTime.Subtract($startTime)
+				Write-Verbose "Time taken: $($timeTaken.TotalSeconds) seconds"
+			}
+			else
+			{
+				Write-TaskInfo -Message "$Destination already exists." -Tag 'Download'
+			}
+		}
+	}
+}
+
+
+
 Export-ModuleMember Invoke-GetAzureBlobContentTask
+Export-ModuleMember Invoke-DownloadAzureBlobContentTask
+
+Register-SitecoreInstallExtension -Command Invoke-DownloadAzureBlobContentTask -As DownloadBlobContent -Type Task
+
 
